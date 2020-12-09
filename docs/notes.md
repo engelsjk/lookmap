7c1911ec510178e75977c266a63e1361139e06e8
engelsjk <jon.engelsman@gmail.com>
2020-12-08 16:06:32 -0800

    add gif and slight project structure change

---
+++ docs/notes.md
@@ -0,0 +1,5 @@
+# related projects
+
+* [Ethermap - A realtime collaborative, version controlled map editor](https://github.com/dwilhelm89/Ethermap)
+* [Real-Time Interaction Between Maps with Socket.io and JavaScript](https://www.youtube.com/watch?v=Xgoexs3xybU)
+* [CrowMapper: Real-time collaborative application for tagging streams of geospatial data, built on top of the Meteor Javascript platform.](https://github.com/TurkServer/CrowdMapper)
--- main.go
+++ main.go
@@ -12,12 +12,12 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/engelsjk/lookmap/lookmap"
+	"github.com/engelsjk/lookmap/socket"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
+func serveMap(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
 	if r.URL.Path != "/" {
 		http.Error(w, "Not found", http.StatusNotFound)
@@ -27,16 +27,16 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	http.ServeFile(w, r, "map.html")
+	http.ServeFile(w, r, "web/map.html")
 }
 
 func main() {
 	flag.Parse()
-	hub := lookmap.NewHub()
+	hub := socket.NewHub()
 	go hub.Run()
-	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/", serveMap)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		lookmap.ServeWs(hub, w, r)
+		socket.ServeWs(hub, w, r)
 	})
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
