d9fe6d31a4d8b988de017915b1cf052baf5c7807
engelsjk <jon.engelsman@gmail.com>
2020-12-08 20:01:01 -0800

    clean up

--- docs/notes.md
+++
@@ -1,5 +0,0 @@
-# related projects
-
-* [Ethermap - A realtime collaborative, version controlled map editor](https://github.com/dwilhelm89/Ethermap)
-* [Real-Time Interaction Between Maps with Socket.io and JavaScript](https://www.youtube.com/watch?v=Xgoexs3xybU)
-* [CrowMapper: Real-time collaborative application for tagging streams of geospatial data, built on top of the Meteor Javascript platform.](https://github.com/TurkServer/CrowdMapper)
--- main.go
+++ main.go
@@ -1,24 +1,17 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// NOTE: lookmap main.go file is currently lightly modified version of the gorilla/websockets/chat example.
-// https://github.com/gorilla/websocket/tree/master/examples/chat
-
 package main
 
 import (
 	"flag"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/engelsjk/lookmap/socket"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var port = flag.String("port", "8080", "http service port")
 
 func serveMap(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
 	if r.URL.Path != "/" {
 		http.Error(w, "Not found", http.StatusNotFound)
 		return
@@ -31,14 +24,21 @@ func serveMap(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
 	flag.Parse()
+
 	hub := socket.NewHub()
+
 	go hub.Run()
+
 	http.HandleFunc("/", serveMap)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		socket.ServeWs(hub, w, r)
 	})
-	err := http.ListenAndServe(*addr, nil)
+
+	log.Printf("running at :%s...", *port)
+
+	err := http.ListenAndServe(net.JoinHostPort("", *port), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
