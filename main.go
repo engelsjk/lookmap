e7a713d2f763638e9127895638614e8ff637b62d
engelsjk <jon.engelsman@gmail.com>
2021-04-15 13:47:56 -0700

    replace main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/engelsjk/lookmap/socket"
+)
+
+var port = flag.String("port", "8080", "http service port")
+
+func main() {
+
+	flag.Parse()
+
+	hub := socket.NewHub()
+
+	go hub.Run()
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		socket.ServeWs(hub, w, r)
+	})
+
+	log.Printf("running at :%s...", *port)
+
+	err := http.ListenAndServe(net.JoinHostPort("", *port), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
